68a4ba9f0542ffb18e4f0d3fde40a467b5b8cb1b
BobiStudio <mgonzgarc@myuax.com>
2023-05-21 02:53:24 +0200

    .

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ coefficients = [8, 3, 5]
 modulos = [13, 11, 8]
 x = teorema_chino_resto(coefficients, modulos)
 for i in range(len(coefficients)):
-    print(coefficients[i], "mod", modulos[i], "=", coefficients[i] % modulos[i])
-print("La solución es:", x)
+    print('x ≡', coefficients[i], '(mod.', modulos[i], ')')
+print('La solución es:', x)
