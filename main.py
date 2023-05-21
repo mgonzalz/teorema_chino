40bdf89457c4485d7e4b8d27d65938d4cf089574
BobiStudio <mgonzgarc@myuax.com>
2023-05-21 11:52:45 +0200

    mas ejemplos

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ modulos = [13, 11, 8]
 sistema(sol, modulos)
 x = teorema_chino_resto(sol, modulos)
 print('La solución es:', x)
+
+
+# Ejercicio de ejemplo
+sol = [2,4,5]
+modulos = [5,7,11]
+sistema(sol, modulos)
+x = teorema_chino_resto(sol, modulos)
+print('La solución es:', x)
+
+# Ejercicio de ejemplo
+sol = [1,10]
+modulos = [7, 11]
+sistema(sol, modulos)
+x = teorema_chino_resto(sol, modulos)
+print('La solución es:', x)
